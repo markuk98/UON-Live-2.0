38c8aeadf8725e14508dc6933c01257647a53d64
markuk98 <mark_uk98@hotmail.com>
2023-04-21 13:00:16 +0100

    #.

---
+++ UON Live 2.0/Pages/ApproveLogins.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+private List<ApplicationUser> Users { get; set; }
+
+protected override async Task OnInitializedAsync()
+{
+    Users = await UserManager.Users.ToListAsync();
+}
+
+private async Task Approve(string userId)
+{
+    var user = await UserManager.FindByIdAsync(userId);
+    user.EmailConfirmed = true;
+    await UserManager.UpdateAsync(user);
+    Users = await UserManager.Users.ToListAsync();
+}
---
+++ UON Live 2.0/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using UON_Live_2._0.Data;
+
+namespace UON_Live_2._0
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
+            });
+        }
+    }
+}
